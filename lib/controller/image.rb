dac571722343b93e5d38380be3f97d2f3682e926
Aleksandr Piatkin <4273812+aspyatkin@users.noreply.github.com>
2024-09-01 20:43:36 +0000

    feat: Fix file permissions

--- lib/controller/image.rb
+++ lib/controller/image.rb
@@ -34,6 +34,7 @@ module VolgaCTF
             image.format('png')
             image_path = ::File.join(::ENV['VOLGACTF_FINAL_TEAM_LOGO_DIR'], "#{team.id}.png")
             image.write(image_path)
+            ::File.chmod(0666, image_path)
             sha256 = ::Digest::SHA256.file(image_path)
 
             ::VolgaCTF::Final::Model::DB.transaction do
