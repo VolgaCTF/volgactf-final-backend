8e07826ac651729cdcaccc84a7aa9b1ad88daa89
Aleksandr Piatkin <4273812+aspyatkin@users.noreply.github.com>
2024-09-01 17:19:36 +0000

    feat: Add web entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,6 @@
 case $1 in
   scheduler) ruby scheduler.rb ;;
   queue) bundle exec sidekiq -r ./lib/queue/tasks.rb -i $2 ;;
+  web) bundle exec thin start -a 0.0.0.0 -p $2 ;;
   *) echo "Unsupported command $1"
 esac
