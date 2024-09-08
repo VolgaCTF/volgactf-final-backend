be6e6ade21e1f318bc1d6ba1857086dc5d894180
Aleksandr Piatkin <4273812+aspyatkin@users.noreply.github.com>
2024-09-08 16:39:18 +0000

    feat: Add entrypoint for cli

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-case $1 in
+command_arg=$1
+shift
+
+case $command_arg in
   scheduler) ruby scheduler.rb ;;
-  queue) bundle exec sidekiq -r ./lib/queue/tasks.rb -i $2 ;;
-  web) bundle exec thin start -a 0.0.0.0 -p $2 ;;
-  *) echo "Unsupported command $1"
+  queue) bundle exec sidekiq -r ./lib/queue/tasks.rb -i $1 ;;
+  web) bundle exec thin start -a 0.0.0.0 -p $1 ;;
+  cli) bundle exec rake "$@" ;;
+  *) echo "Unsupported command $command_arg"
 esac
